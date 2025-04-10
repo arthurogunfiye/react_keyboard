d09f03305d6d688cef072f01bcd259d3983644b2
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-10 06:40:07 +0100

    PR change - rename event method

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ export class App extends React.Component<State> {
     message: '',
   };
 
-  public handleDocumentKeydown = (keyboardEvent: KeyboardEvent) => {
+  public processKeydownEvent = (keyboardEvent: KeyboardEvent) => {
     this.setState({
       message: `The last pressed key is [${keyboardEvent.key}]`,
     });
   };
 
   public componentDidMount(): void {
-    document.addEventListener('keyup', this.handleDocumentKeydown);
+    document.addEventListener('keyup', this.processKeydownEvent);
     this.setState({ message: 'Nothing was pressed yet' });
   }
 
   public componentWillUnmount(): void {
-    document.removeEventListener('keyup', this.handleDocumentKeydown);
+    document.removeEventListener('keyup', this.processKeydownEvent);
   }
 
   public render() {
